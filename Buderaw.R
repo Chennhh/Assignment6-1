5d10e977a28ea72e59df91f80dd95251ae37d4b4
sparkles1 <udochi@bu.edu>
2018-03-26 15:53:17 -0400

    modified code

--- Buderaw.R
+++ Buderaw.R
@@ -2,9 +2,9 @@ library(ggmap)
 library(tidyverse)
 library(magick)
 
-
 gc <- geocode("Bude")
-Wbude<- get_map('Bude',source ="stamen",maptype ="watercolor",zoom = 14)
+Budepath <- get_map("Bude North Cornwall Cricket Club,Bude", zoom = 8)
+Wbude<- get_map(gc,source ="stamen",maptype ="watercolor",zoom = 13,crop=FALSE)
 Rbude <-get_googlemap(center = c(lon = -4.543678, lat = 50.82664),maptype ="roadmap",zoom = 14)
 gc1 <- geocode("Bude Sea Pool,bude")
 gc2 <- geocode("Bude Beaches,bude")
@@ -12,20 +12,33 @@ gc3 <- geocode("Bude North Cornwall Cricket Club,bude")
 gc4 <- geocode("Bar 35,bude")
 
 #merge both rows#
-geocomb <- rbind(gc1,gc2,gc3) %>% 
+geocomb <- rbind(gc1,gc2,gc3)%>% 
   mutate(location = c('Bude Sea Pool','Sandymouth Beach','Bude North Cornwall Cricket Club'),values=30)
-geopath <- rbind(gc3,gc4) %>% 
+geopath <- rbind(gc3,gc4)%>% 
   mutate(location = c('Bude North Cornwall Cricket Club','Bar 35'))
 
 #Watermap#
 watermap <- ggmap(Wbude) +
   geom_point(data = geocomb,
-             aes(x = lon, y = lat),
-             color = "red")+
-  geom_text(data = geocomb,
-            aes(x = lon, y = lat, label = location),
-            color = 'blue')
+             aes(x = lon, y = lat, size=location),
+             color = "red") 
+
 watermap
+
+########### hw 
+############## you can save the map as an image like this:
+dev.copy(png,'watermap.png')
+dev.off()
+
+## or
+
+## you can save the map as data like this:
+saveRDS(watermap, "watermap.RDS")
+
+### in the Rmd file I'll load the map using both methods
+
+
+
 #Roadmap#
 roadmap <- ggmap(Rbude) +
   geom_point(
@@ -37,16 +50,35 @@ roadmap <- ggmap(Rbude) +
 
 roadmap
 
+#### Same as above
+dev.copy(png,'roadmap.png')
+dev.off()
+
+### or
+
+saveRDS(roadmap, "roadmap.RDS")
+
 #images of bude Bude Beaches#
-BudeBeaches <- image_scale(image_read('https://www.visitbude.info/wp-content/uploads/2015/04/Black-Rock-Beach.jpg', "x275"))
+BudeBeaches <- image_scale(image_read('https://www.visitbude.info/wp-content/uploads/2015/04/Black-Rock-Beach.jpg'),geometry_area(400,400))
 print(BudeBeaches)
 
+image_write(BudeBeaches, path = "BudeBeaches.jpg", format = "jpg")
+
 #Images of Bude Sea Pool#
-BudeSeaPool <- image_scale(image_read('https://www.visitengland.com/sites/default/files/styles/experience_page_consumer_gallery_image/public/bude_sea_pool_-_credit_rob_keir.jpg?itok=gDX730Ss', "x275"))
+BudeSeaPool <- image_scale(image_read('https://www.visitengland.com/sites/default/files/styles/experience_page_consumer_gallery_image/public/bude_sea_pool_-_credit_rob_keir.jpg?itok=gDX730Ss'),geometry_area(400,400))
 print(BudeSeaPool)
 
+image_write(BudeSeaPool, "BudeSeaPool.jpg", format="jpg")
+
+#Images of Sandymouth Beach
+SandymouthBeach <- image_scale(image_read('http://www.beachesincornwall.co.uk/wp-content/uploads/2017/04/bude-sandymouth.jpg'),geometry_area(400,400))
+print(SandymouthBeach)
+
+image_write(SandymouthBeach, "SandymouthBeach.jpg", format="jpg")
+
 #Images of Bude North Cornwall Cricket Club#
-Budecricket <- image_scale(image_read('https://www.telegraph.co.uk/content/dam/Travel/galleries/travel/destinations/europe/uk/Englands-most-beautiful-cricket-grounds/cricket-bude_3378037a.jpg?imwidth=1400', "x275"))
+Budecricket <- image_scale(image_read('https://www.telegraph.co.uk/content/dam/Travel/galleries/travel/destinations/europe/uk/Englands-most-beautiful-cricket-grounds/cricket-bude_3378037a.jpg?imwidth=1400'),geometry_area(400,400))
 print(Budecricket)
 
+image_write(Budecricket, "Budecricket.jpg", format = "jpg")
 
